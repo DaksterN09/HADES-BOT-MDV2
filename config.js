a55b6a4fd05ded84d65c5a7d0f3aaa452ac2e8b5
DaksterN09 <110573340+DaksterN09@users.noreply.github.com>
2022-08-03 18:56:09 -0500

    Ownwr cv

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['523532470551'] '𝙾𝙵𝙲 Deyner', true],
+  ['523532470551'], '𝙾𝙵𝙲 Deyner', true],
   ['51934299838'],
   
 ]
